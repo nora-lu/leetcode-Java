52be89ef6d0429d8fa2948c1ba6e4e5fd84c1f75
nora-lu <nora-lu@users.noreply.github.com>
2015-02-27 18:07:50 -0500

    Update and rename 019.RemoveNthNodeFromEndOfList to 019.RemoveNthNodeFromEndOfList/solution2.java

---
+++ 019.RemoveNthNodeFromEndOfList/solution2.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || n == 0) return head;
+        
+        // define a dummy head
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        
+        for (int i = 0; i < n; i++) fast = fast.next;
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
