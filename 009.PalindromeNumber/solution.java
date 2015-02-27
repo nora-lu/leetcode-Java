bd7d4b6ab593ab8ccec448c92d37a8639b47cd32
nora-lu <nora-lu@users.noreply.github.com>
2015-02-27 11:27:28 -0500

    Update solution.java

--- 009.PalindromeNumber/solution.java
+++ 009.PalindromeNumber/solution.java
@@ -2,12 +2,18 @@ public class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0 || (x != 0 && x % 10 == 0)) return false;
         
-        long rev = 0;
-        int xCopy = x;
-        while (x != 0) {
+        // long rev = 0;
+        // int xCopy = x;
+        // while (x != 0) {
+        //     rev = rev * 10 + x % 10;
+        //     x = x / 10;
+        // }
+        // return rev == xCopy;
+        int rev = 0;
+        while (rev < x) {
             rev = rev * 10 + x % 10;
-            x = x / 10;
+            x /= 10;
         }
-        return rev == xCopy;
+        return (rev == x || rev / 10 == x);
     }
 }
