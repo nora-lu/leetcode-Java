9870dc44cf8189c7d0244716466985b48d9f12d6
nora-lu <nora-lu@users.noreply.github.com>
2015-02-27 23:06:53 -0500

    Update solution.java

--- 026.RemoveDuplicatesSortedArray/solution.java
+++ 026.RemoveDuplicatesSortedArray/solution.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public int removeDuplicates(int[] A) {
+        int len = A.length;
+        if (len < 2) return len;
+        
+        int newLen = 0, i = 0;
+        while (i < A.length) {
+            if (A[i] != A[newLen]) {
+                A[++newLen] = A[i];
+            }
+            i++;
+        }
+        return newLen + 1;
+    }
+}
