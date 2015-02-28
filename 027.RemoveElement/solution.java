13228d54983e085f651993211c01840bbdd13d35
nora-lu <nora-lu@users.noreply.github.com>
2015-02-27 23:29:33 -0500

    Update solution.java

--- 027.RemoveElement/solution.java
+++ 027.RemoveElement/solution.java
@@ -1,1 +1,10 @@
-
+public class Solution {
+    public int removeElement(int[] A, int elem) {
+        int newLen = -1, i = 0;
+        while (i < A.length) {
+            if (A[i] != elem) A[++newLen] = A[i];
+            i++;
+        }
+        return newLen + 1;
+    }
+}
