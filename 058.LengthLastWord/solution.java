261b6217cbef3ab4dba3682a0154a9619878dc78
nora-lu <nora-lu@users.noreply.github.com>
2015-02-28 23:27:41 -0500

    Update solution.java

--- 058.LengthLastWord/solution.java
+++ 058.LengthLastWord/solution.java
@@ -1,1 +1,23 @@
-
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        // s = s.trim();
+        
+        // int res = 0;
+        // for (int i = s.length() - 1; i >= 0; i--) {
+        //     if (s.charAt(i) != ' ') res++;
+        //     else break;
+        // }
+        // return res;
+        
+        int res = 0, i = 0;
+        for (i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') break;
+        }
+        while (i >= 0) {
+            if (s.charAt(i) != ' ') res++;
+            else break;
+            i--;
+        }
+        return res;
+    }
+}
