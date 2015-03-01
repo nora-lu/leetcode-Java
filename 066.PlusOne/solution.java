d9c2c67e865ef1b472c80c39a0c9c62b374f54ae
nora-lu <nora-lu@users.noreply.github.com>
2015-03-01 00:13:23 -0500

    Update solution.java

--- 066.PlusOne/solution.java
+++ 066.PlusOne/solution.java
@@ -1,1 +1,18 @@
-
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        if (digits == null || digits.length == 0) return digits;
+        int i = digits.length - 1;
+        while (i >= 0 && digits[i] + 1 == 10) {
+            digits[i] = 0;
+            i--;
+        }
+        if (i == -1) {
+            int[] res = new int[digits.length + 1];
+            res[0] = 1;
+            // for (int j = 1; j < res.length; j++) res[j] = 0;
+            return res;
+        }
+        digits[i] = digits[i] + 1;
+        return digits;
+    }
+}
