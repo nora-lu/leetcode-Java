1347d3e411c98a56aa37de3ec7760db34c0cbabd
nora-lu <nora-lu@users.noreply.github.com>
2015-03-01 11:12:03 -0500

    Recursive Solution

--- 101.SymmetricTree/solution.java
+++ 101.SymmetricTree/solution.java
@@ -1,1 +1,26 @@
-
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        
+        return isSymmetric(root.left, root.right);
+    }
+    
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val == right.val) {
+            return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+        return false;
+    }
+}
