45c767727480019665d7cbbcfc351e90453be371
nora-lu <nora-lu@users.noreply.github.com>
2015-03-01 10:13:49 -0500

    Update solution.java

--- 100.SameTree/solution.java
+++ 100.SameTree/solution.java
@@ -9,8 +9,11 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        else if (p == null || q == null) return false;
+        // if (p == null && q == null) return true;
+        // else if (p == null || q == null) return false;
+        if (p == null || q == null) {
+        return p == null && q == null;
+    } 
         if (p.val == q.val && isSameTree(p.left, q.left)) return isSameTree(p.right, q.right);
         return false;
     }
