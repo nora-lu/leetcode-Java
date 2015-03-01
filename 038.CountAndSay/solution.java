4cfcbe463c2ca84530ecd0d0c40f4d55d2529091
nora-lu <nora-lu@users.noreply.github.com>
2015-02-28 23:03:56 -0500

    Update solution.java

--- 038.CountAndSay/solution.java
+++ 038.CountAndSay/solution.java
@@ -1,1 +1,18 @@
-
+public class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        String res = "";
+        String prev = countAndSay(n - 1);
+        int i = 0, count = 1;
+        while (i < prev.length() - 1) {
+            if (prev.charAt(i) == prev.charAt(i + 1)) count++;
+            else {
+                res = res + count + prev.charAt(i);
+                count = 1;
+            }
+            i++;
+        }
+        res = res + count + prev.charAt(i);
+        return res.toString();
+    }
+}
