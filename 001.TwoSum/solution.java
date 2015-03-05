5e152ac30c5d3d7905e17b87e1cba5c5d0baebd2
nora-lu <nora-lu@users.noreply.github.com>
2015-03-04 19:03:52 -0500

    Update solution.java

--- 001.TwoSum/solution.java
+++ 001.TwoSum/solution.java
@@ -1,1 +1,15 @@
-
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        java.util.Hashtable<Integer, Integer> table = new java.util.Hashtable<Integer, Integer>();
+        int[] res = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            if (table.containsKey(target - numbers[i])) {
+                res[0] = table.get(target - numbers[i]);
+                res[1] = i + 1; // not zero-base;
+                return res;
+            }
+            table.put(numbers[i], i + 1); // not zero-based;
+        }
+        return res;
+    }
+}
