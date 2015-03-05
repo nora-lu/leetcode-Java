9bc8ee2442fbef1acccd7f61ad469f2552cf209f
nora-lu <nora-lu@users.noreply.github.com>
2015-03-05 01:20:28 -0500

    Update solution.java

--- 147.InsertionSortList/solution.java
+++ 147.InsertionSortList/solution.java
@@ -1,1 +1,29 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newHead = new ListNode(0);
+        
+        while (head != null) {
+            ListNode n = newHead;
+            while (n.next != null && n.next.val < head.val) {
+                n = n.next;
+            }
+            ListNode old = head;
+            head = head.next;
+            old.next = n.next;
+            n.next = old;
+        }
+        return newHead.next;
+    }
+}
