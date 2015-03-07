57dced00f4a2736752591a3fb87bde820bb4a826
nora-lu <nora-lu@users.noreply.github.com>
2015-03-06 22:36:44 -0500

    Update solution.java

--- 125.ValidPalindrome/solution.java
+++ 125.ValidPalindrome/solution.java
@@ -1,19 +1,11 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        int len = s.length();
-        if (len < 2) return true;
-        
-        int i = 0, j = len - 1;
+        int i = 0, j = s.length() - 1;
         while (i < j) {
             while (!Character.isLetterOrDigit(s.charAt(i)) && i < j) i++;
-            if (i == j) return true;
-            
             while (!Character.isLetterOrDigit(s.charAt(j)) && i < j) j--;
-            if (i == j) return true;
-            
             if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
-            i++;
-            j--;
+            i++; j--;
         }
         return true;
     }
