556f568ae4310515064b9098b7cf529862adac02
nora-lu <nora-lu@users.noreply.github.com>
2015-04-10 23:18:20 -0400

    Update solution.java

--- 168.ExcelSheetColumnTitle/solution.java
+++ 168.ExcelSheetColumnTitle/solution.java
@@ -1,5 +1,13 @@
 public class Solution {
     public String convertToTitle(int n) {
         return n == 0 ? "" : convertToTitle(--n / 26) + (char)('A' + (n % 26));
+        
+        // StringBuilder builder = new StringBuilder();
+        // while (n > 0) {
+        //     n--;  // pay attention to here
+        //     builder.insert(0, (char) ('A' + n % 26));
+        //     n /= 26;
+        // }
+        // return builder.toString();
     }
 }
