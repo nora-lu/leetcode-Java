bbf5d78b76b583638db92d45aeb5788fb6ee4ea8
nora-lu <nora-lu@users.noreply.github.com>
2015-04-10 23:27:39 -0400

    Update solution.java

--- 169.MajorityElement/solution.java
+++ 169.MajorityElement/solution.java
@@ -1,1 +1,18 @@
-
+public class Solution {
+    public int majorityElement(int[] num) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < num.length; i++) {
+            int element = num[i];
+            if (map.containsKey(element)) map.put(element, map.get(element) + 1);
+            else map.put(element, 1);
+        }
+        int major = 0;
+        for (int i : map.keySet()) {
+            if (map.get(i) > num.length / 2) {
+                major = i;
+                break;
+            }
+        }
+        return major;
+    }
+}
