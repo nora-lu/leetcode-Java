b0e7e2a11d6c92224cf51c5f9f6bd757f3047001
nora-lu <nora-lu@users.noreply.github.com>
2015-04-19 12:32:17 -0400

    Update solution.java

--- 036.ValidSudoku/solution.java
+++ 036.ValidSudoku/solution.java
@@ -1,1 +1,27 @@
-
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        if (board == null) return false;
+        // check every row & column
+        for (int i = 0; i < 9; i++) {
+            if (!isValid(board, i, 0, i, 8)) return false;
+            if (!isValid(board, 0, i, 8, i)) return false;
+        }
+        // check every grid
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (!isValid(board, i*3, j*3, i*3+2, j*3+2)) return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int x1, int y1, int x2, int y2) {
+        Set digits = new HashSet();
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                if (board[i][j] != '.' && !digits.add(board[i][j])) return false;
+            }
+        }
+        return true;
+    }
+}
