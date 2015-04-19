f2fc1c024ee92ce3dd30951e1f524fbb8c3bc62f
nora-lu <nora-lu@users.noreply.github.com>
2015-04-18 21:26:37 -0400

    Update solution.java

--- 118.Pascal-sTriangle/solution.java
+++ 118.Pascal-sTriangle/solution.java
@@ -1,1 +1,14 @@
-
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
+        List<Integer> row = new ArrayList<Integer>();
+        for (int i = 0; i < numRows; i++) {
+            row.add(0, 1);
+            for (int j = 1; j < row.size() - 1; j++) {
+                row.set(j, row.get(j) + row.get(j + 1));
+            }
+            triangle.add(new ArrayList<Integer>(row));
+        }
+        return triangle;
+    }
+}
