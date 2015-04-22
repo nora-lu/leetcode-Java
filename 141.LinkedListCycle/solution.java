02ea3b9cd4127dbc0bd666c61a557bee3b6adf86
nora-lu <nora-lu@users.noreply.github.com>
2015-04-21 21:40:26 -0400

    Two Runners Solution

---
+++ 141.LinkedListCycle/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // set two runners
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        // fast runner move 2 steps at one time while slow runner move 1 step,
+        // if traverse to a null, there must be no loop
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
