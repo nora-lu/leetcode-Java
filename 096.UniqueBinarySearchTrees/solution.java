e8a878e097d11f9907f402f2f6f6abbcc1c7ae07
nora-lu <nora-lu@users.noreply.github.com>
2015-04-24 20:21:09 -0400

    Update solution.java

--- 096.UniqueBinarySearchTrees/solution.java
+++ 096.UniqueBinarySearchTrees/solution.java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    public int numTrees(int n) {
+        int[] cache = new int[n+1];
+        cache[0] = cache[1] = 1;
+        
+        for (int i=2; i<=n; i++) {
+            for (int j=0; j<i; j++) {
+                cache[i] += cache[j] * cache[i-j-1];
+            }
+        }
+        return cache[n];
+    }
+}
