a268873b16a44edd106e89f1bdfc19e1444e35d4
nora-lu <nora-lu@users.noreply.github.com>
2015-04-25 17:36:20 -0400

    Create solution.java

---
+++ 035.SearchInsertPosition/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target == nums[mid]) return mid;
+            else if (target > nums[mid]) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return lo;
+    }
+}
