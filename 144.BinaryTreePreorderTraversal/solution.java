8e3169e59b15dd0cd130d121324c4c3bdc12c30d
nora-lu <nora-lu@users.noreply.github.com>
2015-04-24 20:26:03 -0400

    Recursive Solution

---
+++ 144.BinaryTreePreorderTraversal/solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        result.add(root.val);
+        result.addAll(preorderTraversal(root.left));
+        result.addAll(preorderTraversal(root.right));
+        return result;
+    }
+}
