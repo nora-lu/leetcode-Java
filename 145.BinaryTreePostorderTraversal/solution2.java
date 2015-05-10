f0c9b774f9bc90bbcbd216607ddd0043fe859f53
nora-lu <nora-lu@users.noreply.github.com>
2015-05-10 19:55:10 -0400

    Update solution2.java

--- 145.BinaryTreePostorderTraversal/solution2.java
+++ 145.BinaryTreePostorderTraversal/solution2.java
@@ -18,24 +18,13 @@ public class Solution {
         
         stack.push(root);
         while (!stack.empty()) {
-            /**
-             * We use a prev variable to keep track of the previously-traversed node. Let’s assume curr is the current node                * that’s on top of the stack. When prev is curr‘s parent, we are traversing down the tree. In this case, we try               * to traverse to curr‘s left child if available (ie, push left child to the stack). If it is not available, we                * look at curr‘s right child. If both left and right child do not exist (ie, curr is a leaf node), we print                   * curr‘s value and pop it off the stack.
-             */
             cur = stack.peek();
             if (prev == null || prev.left == cur || prev.right == cur) { // traverse down the tree
                 if (cur.left != null) { stack.push(cur.left); }
                 else if (cur.right != null) { stack.push(cur.right); }
-            } 
-            /**
-             * If prev is curr‘s left child, we are traversing up the tree from the left. We look at curr‘s right child. If it              * is available, then traverse down the right child (ie, push right child to the stack), otherwise print curr‘s                * value and pop it off the stack.
-             */
-            else if (cur.left == prev) { // traverse up the tree from the left
+            } else if (cur.left == prev) { // traverse up the tree from the left
                 if (cur.right != null) { stack.push(cur.right); }
-            } 
-            /**
-             * If prev is curr‘s right child, we are traversing up the tree from the right. In this case, we print curr‘s                  * value and pop it off the stack.
-             */
-            else { // traverse up the tree from the right
+            } else { // traverse up the tree from the right
                 res.add(cur.val);
                 stack.pop();
             }
