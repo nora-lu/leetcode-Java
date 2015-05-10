d18d1486d60dc3e4d19260ad15a655c24fa8b155
nora-lu <nora-lu@users.noreply.github.com>
2015-05-10 18:18:15 -0400

    Create solution.java

---
+++ 094.BinaryTreeInorder/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) { return res; }
+        
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
