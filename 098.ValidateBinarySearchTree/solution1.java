8f2d21a4b508abcf0921e87d2a76042c1ab6fc37
nora-lu <nora-lu@users.noreply.github.com>
2015-05-11 17:03:19 -0400

    Recursive Solution

---
+++ 098.ValidateBinarySearchTree/solution1.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    
+    private static boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) { return true; }
+        if (node.val >= max || node.val <= min) { return false; }
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
