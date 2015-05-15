ab0d72c17e8ab53e0bbb6622f3c0abb2219f1da4
nora-lu <nora-lu@users.noreply.github.com>
2015-05-15 16:43:20 -0400

    Update solution.java

--- 064.MinimumPathSum/solution.java
+++ 064.MinimumPathSum/solution.java
@@ -1,22 +1,22 @@
 public class Solution {
     public int minPathSum(int[][] grid) {
-        if (grid == null || grid.length == 0) { return 0; }
+        if (grid == null || grid.length == 0 || grid[0].length == 0) { return 0; }
         
         int m = grid.length, n = grid[0].length;
         int[][] minSum = new int[m][n];
         minSum[0][0] = grid[0][0];
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j > 0) { // first row, only has path from its left cell
-                    minSum[i][j] = minSum[i][j - 1] + grid[i][j];
-                }
-                if (j == 0 && i > 0) { // first column, only has path from its upper cell
-                    minSum[i][j] = minSum[i - 1][j] + grid[i][j];
-                }
-                if (i > 0 && j > 0) {
-                    minSum[i][j] = Math.min(minSum[i][j - 1], minSum[i - 1][j]) + grid[i][j];
-                }
+        for (int i = 1; i < m; i++) { // update first column except first cell
+            minSum[i][0] = minSum[i - 1][0] + grid[i][0];
+        }
+        
+        for (int j = 1; j < n; j++) { // update first row except first cell
+            minSum[0][j] = minSum[0][j - 1] + grid[0][j];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                minSum[i][j] = Math.min(minSum[i][j - 1], minSum[i - 1][j]) + grid[i][j];
             }
         }
         return minSum[m - 1][n - 1];
