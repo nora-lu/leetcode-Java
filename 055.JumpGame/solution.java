eaa4b975afdeaeb3ff661f80763bddb75cb645f1
nora-lu <nora-lu@users.noreply.github.com>
2015-05-16 14:18:46 -0400

    Update solution.java

--- 055.JumpGame/solution.java
+++ 055.JumpGame/solution.java
@@ -1,1 +1,9 @@
-
+public class Solution {
+    public boolean canJump(int[] nums) {
+        for (int i = 0, maxReach = 0; i < nums.length && i <= maxReach; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) { return true; }
+        }
+        return false;
+    }
+}
