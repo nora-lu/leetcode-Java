ffd5670d7bcdf3157032ba60f29daba4c1e4a23c
nora-lu <nora-lu@users.noreply.github.com>
2015-05-16 18:29:33 -0400

    Update solution.java

--- 090.SubsetsII/solution.java
+++ 090.SubsetsII/solution.java
@@ -1,1 +1,21 @@
-
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> subsets = new ArrayList<List<Integer>>();
+        subsets.add(new ArrayList<Integer>());
+        Arrays.sort(nums);
+        
+        int size = 0, startIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+          // If we want to insert an element which is a dup, we can only insert it
+          // after the newly inserted elements from last step.
+            startIndex = (i > 0 && nums[i] == nums[i - 1]) ? size : 0;
+            size = subsets.size();
+            for (int j = startIndex; j < size; j++) {
+                List<Integer> subset = new ArrayList<Integer>(subsets.get(j));
+                subset.add(nums[i]);
+                subsets.add(subset);
+            }
+        }
+        return subsets;
+    }
+}
