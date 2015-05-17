ecf478b6a831a98a267c89c127e7331e56a22d11
nora-lu <nora-lu@users.noreply.github.com>
2015-05-17 17:09:24 -0400

    Update solution.java

--- 162.FindPeakElement/solution.java
+++ 162.FindPeakElement/solution.java
@@ -1,1 +1,13 @@
-
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        if (nums == null) { return 0; }
+        int lo = 0, hi = nums.length - 1;
+        int mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] < nums[mid + 1]) { lo = mid + 1; }
+            else { hi = mid; }
+        }
+        return lo;
+    }
+}
