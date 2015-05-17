ef0d11953740a8e679addeb4f8536fededf04127
nora-lu <nora-lu@users.noreply.github.com>
2015-05-17 12:10:40 -0400

    Update solution.java

--- 046.Permutations/solution.java
+++ 046.Permutations/solution.java
@@ -1,1 +1,24 @@
-
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums == null) { return null; }
+        return permute(nums, nums.length - 1);
+    }
+    
+    private List<List<Integer>> permute(int[] nums, int end) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if (nums.length == 0 || end < 0) {
+            res.add(new ArrayList<Integer>());
+            return res;
+        }
+        List<List<Integer>> subResult = permute(nums, end - 1);
+        for (List<Integer> list : subResult) {
+            for (int i = 0; i <= list.size(); i++) {
+                List<Integer> newPerm = new ArrayList<Integer>(list);
+                newPerm.add(i, nums[end]);
+                res.add(newPerm);
+            }
+        }
+        return res;
+    }
+}
