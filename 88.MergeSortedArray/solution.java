194af0715c5578bb086b0647da0ea6b81bcfd5ce
Qian <desireemikan@gmail.com>
2015-05-18 16:09:33 -0400

    more

--- 88.MergeSortedArray/solution.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public void merge(int A[], int m, int B[], int n) {
-        for (int k = m + n - 1, i = m - 1, j = n - 1; k >= 0; k--) {
-            if (i >= 0 && j >= 0) A[k] = A[i] > B[j] ? A[i--] : B[j--];
-            else if (j >= 0) A[k] = B[j--];
-        }
-    }
-}
