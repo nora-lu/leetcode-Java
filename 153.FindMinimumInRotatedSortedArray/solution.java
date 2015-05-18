88f7feb770692761a56d1b3ecb27b131f6dba4a9
nora-lu <nora-lu@users.noreply.github.com>
2015-05-18 14:15:46 -0400

    Update solution.java

--- 153.FindMinimumInRotatedSortedArray/solution.java
+++ 153.FindMinimumInRotatedSortedArray/solution.java
@@ -2,8 +2,8 @@ public class Solution {
     public int findMin(int[] nums) {
         if (nums == null) { return Integer.MIN_VALUE; }
 
-		int lo = 0, hi = nums.length - 1;
-		while (lo < hi) {
+				int lo = 0, hi = nums.length - 1;
+			while (lo < hi) {
 		  // if the first member is less than the last member, 
 		  // there's no rotation in the array. 
 		  if (nums[lo] < nums[hi]) {
