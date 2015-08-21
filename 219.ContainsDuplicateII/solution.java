0c0919613dd53d400bc3bce1d80fecffbc9a3732
nora-lu <nora-lu@users.noreply.github.com>
2015-08-21 19:52:31 -0400

    Update solution.java

--- 219.ContainsDuplicateII/solution.java
+++ 219.ContainsDuplicateII/solution.java
@@ -2,12 +2,19 @@ public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if (nums == null) { return false; }
         Map<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     int n = nums[i];
+        //     if (map.containsKey(n) && i - map.get(n) <= k) {
+        //         return true;
+        //     } else {
+        //         map.put(n, i);
+        //     }
+        // }
         for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            if (map.containsKey(n) && i - map.get(n) <= k) {
+            // the previous value associated with key, or null if there was no mapping for key.
+            Integer ord = map.put(nums[i], i);
+            if (ord != null && i - ord <= k) {
                 return true;
-            } else {
-                map.put(n, i);
             }
         }
         return false;
