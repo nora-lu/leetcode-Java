c9501c61ee412c85a41b5eeb90fb1376fdf235cb
nora-lu <nora-lu@users.noreply.github.com>
2015-08-22 12:34:20 -0400

    Update mysolution.java

--- 205.IsomorphicStrings/mysolution.java
+++ 205.IsomorphicStrings/mysolution.java
@@ -7,6 +7,9 @@ public class Solution {
             Character c2 = t.charAt(i);
             Integer lastPos1 = map1.put(c1, i);
             Integer lastPos2 = map2.put(c2, i);
+            // if both lastPos1, lastPos2 are null, continue;
+            // if one of them is null, return false;
+            // if their values are not equal, return false;
             if (lastPos1 != null || lastPos2 != null) {
                 if (lastPos1 == null || lastPos2 == null || lastPos1.compareTo(lastPos2) != 0) {
                     return false;
