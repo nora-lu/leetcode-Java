c17be61c043b83c166255efbee4555354c887a74
nora-lu <nora-lu@users.noreply.github.com>
2015-08-22 12:51:24 -0400

    Create noHashMapSolution.java

---
+++ 205.IsomorphicStrings/noHashMapSolution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) { return false; }
+            // Attention: the array is initialized to all 0's.
+            m1[s.charAt(i)] = i + 1;
+            m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
+}
