e2c994eea22c5d50407b7468fe76357691c6ef9e
nora-lu <nora-lu@users.noreply.github.com>
2015-08-27 17:57:09 -0400

    Cycle Detection solution.

---
+++ 202.HappyNumber/solution2.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(fast);
+            fast = digitSquareSum(fast);
+        } while (slow != fast);
+        if (slow == 1) { return true; }
+        return false;
+    }
+    
+    private int digitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
+    }
+}
