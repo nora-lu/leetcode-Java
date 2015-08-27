07e4950bbf060f75a5ada2138c3faa39de99f7e2
nora-lu <nora-lu@users.noreply.github.com>
2015-08-27 11:58:23 -0400

    Create solution.java

---
+++ 053.MaximumSubarray/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums == null || nums.length == 0) { return 0; }
+
+        int max = nums[0];  // global max sum
+        int maxNow = nums[0];  
+        for (int i = 1; i < nums.length; i++) {
+            maxNow = Math.max(maxNow + nums[i], nums[i]);
+            max = Math.max(max, maxNow);
+        }
+        return max;
+    }
+}
