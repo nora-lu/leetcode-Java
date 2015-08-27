4a9f68ecf1e0ba243b06e81722807752a71b0ad5
nora-lu <nora-lu@users.noreply.github.com>
2015-08-27 15:46:22 -0400

    One pass solution.

--- 075.SortColors/solution2.java
+++ 075.SortColors/solution2.java
@@ -2,6 +2,7 @@ public class Solution {
     public void sortColors(int[] nums) {
         int zero = 0, two = nums.length - 1;
         for (int i = 0; i <= two; i++) {
+            //
             while (nums[i] == 2 && i < two) { swap(nums, i, two--); }
             while (nums[i] == 0 && i > zero) { swap(nums, i, zero++); }
         }
