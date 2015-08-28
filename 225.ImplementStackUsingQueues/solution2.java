9fa28fad1e3325fcf41f4585e4c240ca8b7c5394
nora-lu <nora-lu@users.noreply.github.com>
2015-08-28 17:47:08 -0400

    Create solution2.java

---
+++ 225.ImplementStackUsingQueues/solution2.java
@@ -0,0 +1,25 @@
+class MyStack {
+    Queue<Integer> q = new LinkedList<>();
+    // Push element x onto stack.
+    public void push(int x) {
+        q.offer(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.offer(q.poll());
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void pop() {
+        q.poll();
+    }
+
+    // Get the top element.
+    public int top() {
+        return q.peek();
+    }
+
+    // Return whether the stack is empty.
+    public boolean empty() {
+        return q.size() == 0;
+    }
+}
