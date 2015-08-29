55d3b209546378eca13906e85c819da0976f3bf6
nora-lu <nora-lu@users.noreply.github.com>
2015-08-29 14:30:56 -0400

    Create solution.java

---
+++ 198.HouseRobber/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null) { return 0; }
+        
+        int prevYes = 0;    // max monney can get if rob current house
+        int prevNo = 0;     // max money can get if not rob current house
+        for (int i = 0; i < nums.length; i++) {
+            // If we rob current cell, previous cell shouldn't be robbed. So, add the current value to              // previous one.
+            int curYes = nums[i] + prevNo;
+            // If we don't rob current cell, then the count should be max of the previous cell robbed               // and not robbed
+            int curNo = Math.max(prevYes, prevNo);
+            
+            prevYes = curYes;
+            prevNo = curNo;
+        }
+        return Math.max(prevYes, prevNo);
+    }
+}
