59b45d257209732f2ba224769aced1eb96f2d0b2
nora-lu <nora-lu@users.noreply.github.com>
2015-09-04 13:02:43 -0400

    Non-recursive solution without Stack.

---
+++ 114.FlattenBinaryTreeToLinkedList/solution3.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void flatten(TreeNode root) {
+        while (root != null) {
+            if (root.left != null) {
+                TreeNode pre = root.left;
+                while (pre.right != null) { pre = pre.right; }
+                pre.right = root.right;
+                root.right = root.left;
+                root.left = null;
+            }
+            root = root.right;
+        }
+        
+    }
+}
