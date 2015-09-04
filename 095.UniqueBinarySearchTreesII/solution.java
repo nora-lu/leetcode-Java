63b6e4888416aa813972d1301cb1c50793df70f4
nora-lu <nora-lu@users.noreply.github.com>
2015-09-04 18:43:52 -0400

    My Recursive solution.

---
+++ 095.UniqueBinarySearchTreesII/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return generateBST(1, n);
+    }
+    
+    private List<TreeNode> generateBST(int start, int end) {
+        List<TreeNode> res = new ArrayList<>();
+        if (start > end) {
+            res.add(null);
+        } else if (start == end) {
+            res.add(new TreeNode(start));
+        } else {
+            for (int i = start; i <= end; i++) {
+                List<TreeNode> left = generateBST(start, i - 1);
+                List<TreeNode> right = generateBST(i + 1, end);
+                for (TreeNode l : left) {
+                    for (TreeNode r : right) {
+                        TreeNode root = new TreeNode(i);
+                        root.left = l;
+                        root.right = r;
+                        res.add(root);
+                    }
+                }
+            }
+            
+        }
+        return res;
+    }
+}
