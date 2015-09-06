0ed7395dd41e8294e57ce71537c2e6eee2f7278c
nora-lu <nora-lu@users.noreply.github.com>
2015-09-06 13:18:20 -0400

    Create mysolution.java

---
+++ 240.Search2D_MatrixII/mysolution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null) { return false; }
+        int m = matrix.length, n = matrix[0].length;
+        // find index of last row with first number <= target
+        int lo = 0, hi = m - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            int midVal = matrix[mid][0];
+            if (midVal == target) { return true; }
+            else if (midVal > target) { hi = mid - 1; }
+            else { lo = mid; }
+        }
+        
+        int rowMax = lo;
+        for (int i = 0; i <= rowMax; i++) {
+            lo = 0; hi = n - 1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                int midVal = matrix[i][mid];
+                if (midVal == target) { return true; }
+                else if (midVal > target) { hi = mid - 1; }
+                else { lo = mid + 1; }
+            }
+        }
+        return false;
+    }
+}
