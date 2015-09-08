5779d921082adff1ef066ed190eeeb1bea5dda85
nora-lu <nora-lu@users.noreply.github.com>
2015-09-08 10:57:30 -0400

    Create mysolution.java

---
+++ 016.3SumClosest/mysolution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int lo = i + 1, hi = nums.length - 1;
+                while (lo < hi) {
+                    int sum = nums[i] + nums[lo] + nums[hi];
+                    if (sum == target) { return sum; }
+                    if (Math.abs(sum - target) < Math.abs(closest - target)) {
+                        closest = sum;
+                    }
+                    if (sum < target) { lo++; }
+                    else { hi--; }
+                }
+            }
+        }
+        return closest;
+    }
+}
