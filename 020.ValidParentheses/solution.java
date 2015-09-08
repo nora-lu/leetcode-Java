9a4574f9da2a91390b20f49cbc8eecbf7427a86d
nora-lu <nora-lu@users.noreply.github.com>
2015-09-08 11:57:16 -0400

    Update solution.java

--- 020.ValidParentheses/solution.java
+++ 020.ValidParentheses/solution.java
@@ -1,1 +1,40 @@
+public class Solution {
+    // public boolean isValid(String s) {
+    //     if (s.length() % 2 != 0) return false; 
+        
+    //     ArrayList<Character> stack = new ArrayList<>();
+    //     boolean result = true;
+    //     int i = 0;
+    //     while (result && i < s.length()) {
+    //         Character c = s.charAt(i);
+    //         if (c == '(' || c == '[' || c == '{') stack.add(c);
+    //         else if (stack.size() > 0) {
+    //             if (c == ')') result = stack.remove(stack.size() - 1).equals('(');
+    //             else if (c == ']') result = stack.remove(stack.size() - 1).equals('[');
+    //             else if (c == '}') result = stack.remove(stack.size() - 1).equals('{');
+    //         }
+    //         else result = false;
+    //         i++;
+    //     }
+    //     return stack.size() == 0 && result;
+    // }
+    private static final Map<Character, Character> map = new HashMap<Character, Character>() {
+        {
+            put('(', ')');
+            put('[', ']');
+            put('{', '}');
+        }
+    };
 
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else if (stack.empty() || map.get(stack.pop()) != c) {
+                return false;
+            }
+        }
+        return stack.empty();
+    }
+}
