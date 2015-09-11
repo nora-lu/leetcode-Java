11cd2d355214facebe7ff354c52d19cc7dadd4f5
nora-lu <nora-lu@users.noreply.github.com>
2015-09-11 11:02:11 -0400

    Update solution.java

--- 006.ZigZagConversion/solution.java
+++ 006.ZigZagConversion/solution.java
@@ -1,1 +1,21 @@
-
+public class Solution {
+    public String convert(String s, int numRows) {
+        int len = s.length();
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i = 0; i < numRows; i++) { sb[i] = new StringBuffer(); }
+        
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) {
+                sb[idx].append(s.charAt(i++));
+            }
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--) {
+                sb[idx].append(s.charAt(i++));
+            }
+        }
+        for (int idx = 1; idx < numRows; idx++) {
+            sb[0].append(sb[idx]);
+        }
+        return sb[0].toString();
+    }
+}
