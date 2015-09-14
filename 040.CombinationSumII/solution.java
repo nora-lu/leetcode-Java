98bcac900799595898168b44949c3ab7fe7683ff
nora-lu <nora-lu@users.noreply.github.com>
2015-09-14 11:15:17 -0400

    Create solution.java

---
+++ 040.CombinationSumII/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        helper(candidates, 0, target, new ArrayList<Integer>(), res);
+        return res;
+    }
+    
+    private void helper(int[] candidates, int start, int target, List<Integer> list, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(list);
+        }
+        else {
+            int i = start;
+            while (i < candidates.length) {
+                if (i == start || candidates[i] != candidates[i - 1]) {
+                    int newTarget = target - candidates[i];
+                    if (newTarget >= 0) {
+                        List<Integer> copy = new ArrayList<>(list);
+                        copy.add(candidates[i]);
+                        helper(candidates, i + 1, newTarget, copy, res);
+                    } else { break; }
+                }
+                i++;
+            }
+        }
+    }
+}
