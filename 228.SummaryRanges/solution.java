4e0b0597047af3a163b7948e56e8343a773fa5a7
nora-lu <nora-lu@users.noreply.github.com>
2015-09-14 08:17:56 -0400

    Create solution.java

---
+++ 228.SummaryRanges/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> summary = new ArrayList<>();
+        if (nums == null || nums.length == 0) { return summary; }
+        int i = 0;
+        while (i < nums.length) {
+            int j = i;
+            while (j < nums.length - 1 && nums[j] + 1 == nums[j + 1]) { j++; }
+            if (i == j) { summary.add("" + nums[i]); }
+            else { summary.add(nums[i] + "->" + nums[j]); }
+            i = j + 1;
+        }
+        return summary;
+    }
+}
