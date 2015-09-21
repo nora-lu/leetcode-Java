84afb238bf85cb2c82d4c6efea6220da7e33208f
nora-lu <nora-lu@users.noreply.github.com>
2015-09-21 12:03:43 -0400

    Update BFSsolution.java

--- 210.CourseScheduleII/BFSsolution.java
+++ 210.CourseScheduleII/BFSsolution.java
@@ -1,39 +1,42 @@
 public class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         int[] order = new int[numCourses];
+        // special corner case
         if (prerequisites == null || prerequisites.length == 0) {
             for (int i = 0; i < numCourses; i++) { order[i] = i; }
             return order;
         }
         
         int len = prerequisites.length;
-        int[] numPre = new int[numCourses];
-        for (int i = 0; i < len; i++) {
-            numPre[prerequisites[i][0]]++;
+        
+        List<List<Integer>> adj = new ArrayList<>(numCourses);  // store the graph
+        for (int i = 0; i < numCourses; i++) { adj.add(new ArrayList<>()); }
+        
+        int[] numPre = new int[numCourses];  // record number of pre for each course
+        for (int[] edge : prerequisites) {
+            numPre[edge[0]]++;
+            adj.get(edge[1]).add(edge[0]);  // 1->2, 1 must be taken before 2.
         }
         
         Queue<Integer> noPre = new LinkedList<>();
+        // add courses not need pre to Queue
         for (int i = 0; i < numCourses; i++) {
             if (numPre[i] == 0) {
                 noPre.offer(i);
             }
         }
-        int orderIdx = 0;
+        
+        int visited = 0;
         while (!noPre.isEmpty()) {
-            int course = noPre.poll();
-            order[orderIdx++] = course;
-            for (int i = 0; i < len; i++) {
-                if (prerequisites[i][1] == course) {
-                    numPre[prerequisites[i][0]]--;
-                    if (numPre[prerequisites[i][0]] == 0) {
-                        noPre.offer(prerequisites[i][0]);
-                    }
+            int from = noPre.poll();
+            order[visited++] = from;
+            for (int to : adj.get(from)) {
+                numPre[to]--;
+                if (numPre[to] == 0) {
+                    noPre.offer(to);
                 }
             }
         }
-        if (orderIdx != numCourses) {  // impossible to finish
-            return new int[]{};
-        }
-        return order;
+        return visited == numCourses ? order : new int[0];
     }
 }
