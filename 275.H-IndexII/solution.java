a211878080ec12115bfce03837f6c3b25b7e5e50
nora-lu <nora-lu@users.noreply.github.com>
2015-09-21 14:32:55 -0400

    Update solution.java

--- 275.H-IndexII/solution.java
+++ 275.H-IndexII/solution.java
@@ -3,13 +3,13 @@ public class H-IndexII {
     if (citations == null || citations.length == 0) { return 0; }
     int len = citations.length;
     int lo = 0, hi = len - 1;
-    while (lo < hi) {
+    while (lo <= hi) {
       int mid = lo + (hi - lo) / 2;
       int val = citations[mid];
       if (val == len - mid) { return val; }
       else if (val < len - mid) { lo = mid + 1; }
-      else { hi = mid; }
+      else { hi = mid - 1; }
     }
-    return citations[lo] >= len - lo ? len - lo : 0;
+    return len - lo;
   }
 }
