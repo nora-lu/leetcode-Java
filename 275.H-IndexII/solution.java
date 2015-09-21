36c0744f28053aafa7e3092a49b7ce711aaad2ad
nora-lu <nora-lu@users.noreply.github.com>
2015-09-21 14:29:02 -0400

    Create solution.java

---
+++ 275.H-IndexII/solution.java
@@ -0,0 +1,15 @@
+public class H-IndexII {
+  public int hIndex(int[] citations) {
+    if (citations == null || citations.length == 0) { return 0; }
+    int len = citations.length;
+    int lo = 0, hi = len - 1;
+    while (lo < hi) {
+      int mid = lo + (hi - lo) / 2;
+      int val = citations[mid];
+      if (val == len - mid) { return val; }
+      else if (val < len - mid) { lo = mid + 1; }
+      else { hi = mid; }
+    }
+    return citations[lo] >= len - lo ? len - lo : 0;
+  }
+}
