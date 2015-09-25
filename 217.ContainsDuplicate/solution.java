4a2ec1c0fc98c1a18fbb7b325fe3a607450ddf0f
nora-lu <nora-lu@users.noreply.github.com>
2015-09-25 18:12:01 -0400

    Update solution.java

--- 217.ContainsDuplicate/solution.java
+++ 217.ContainsDuplicate/solution.java
@@ -1,13 +1,11 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
+        if (nums == null) { return false; }
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
-                return true;
-            } else {
-                set.add(nums[i]);
-            }
+        for (int i : nums) {
+            if (!set.add(i)) { return true; }
         }
         return false;
+
     }
 }
