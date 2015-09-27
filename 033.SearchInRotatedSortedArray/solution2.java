3c69f936f419c969ac4825c9c8d55d3f6b38d4eb
nora-lu <nora-lu@users.noreply.github.com>
2015-09-27 16:49:46 -0400

    Create solution2.java

---
+++ 033.SearchInRotatedSortedArray/solution2.java
@@ -0,0 +1,24 @@
+public class SearchInRotatedSortedArray {
+  public int search(int[] nums, int target) {
+    int minIdx = findMinIdx(nums);
+    int lo = 0, hi = nums.length - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      int realMid = (mid + minIdx) % nums.length;
+      if (nums[realMid] == target) { return realMid; }
+      if (nums[realMid] < target) { lo = mid + 1; }
+      else { hi = mid - 1; }
+    }
+    return -1;
+  }
+
+  private int findMinIdx(int[] nums) {
+    int lo = 0, hi = nums.length - 1;
+    while (lo < hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (nums[mid] > nums[hi]) { lo = mid + 1; }
+      else hi = mid;
+    }
+    return lo;
+  }
+}
