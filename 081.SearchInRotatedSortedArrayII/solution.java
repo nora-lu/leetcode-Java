90725ccf9c77bf2a1e50efe719f41eaa1c8d5698
nora-lu <nora-lu@users.noreply.github.com>
2015-09-27 17:27:40 -0400

    Create solution.java

---
+++ 081.SearchInRotatedSortedArrayII/solution.java
@@ -0,0 +1,17 @@
+public class SearchInRotatedSortedArrayII {
+  public boolean search(int[] nums, int target) {
+    int lo = 0, hi = nums.length - 1;
+    while (lo <= hi) {
+      int mid = lo + (hi - lo) / 2;
+      if (nums[mid] == target) { return true; }
+      if (nums[lo] < nums[mid]) {  // left half is sorted
+        if (nums[lo] <= target && target < nums[mid]) { hi = mid - 1; }
+        else { lo = mid + 1; }
+      } else if (nums[lo] > nums[mid]) {  // right half is sorted
+        if (nums[mid] < target && target <= nums[hi]) { lo = mid + 1; }
+        else { hi = mid - 1; }
+      } else { lo++; }
+    }
+    return false;
+  }
+}
