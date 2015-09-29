e9627a477a31effdbfd89550ac5ff054e2de996f
nora-lu <nora-lu@users.noreply.github.com>
2015-09-29 14:38:36 -0400

    Create solution.java

---
+++ 139.WordBreak/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        if (s == null) { return true; }
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[0] = true;
+        for (int i = 1; i <= len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordDict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
