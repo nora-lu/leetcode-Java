2f30609504cefad49eb9d5fad9a252ad449980dd
nora-lu <nora-lu@users.noreply.github.com>
2015-09-29 21:47:32 -0400

    Update mysolution.java

--- 060.PermutationSequence/mysolution.java
+++ 060.PermutationSequence/mysolution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public String getPermutation(int n, int k) {
         StringBuilder sb = new StringBuilder();
-        List<Integer> candidates = new ArrayList<>();
+        List<Integer> candidates = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             candidates.add(i);
         }
+        
+        int[] fact = new int[n];
+        fact[0] = 1;
+        for (int i = 1; i < n; i++) {
+            fact[i] = i * fact[i - 1];
+        }
+        
         while (sb.length() < n) {
-            int ft = factorial(candidates.size() - 1);
+            int ft = fact[candidates.size() - 1];
             int idx = (k - 1) / ft;
             sb.append(candidates.get(idx));
             candidates.remove(idx);
             k = k - ft * idx;
         }
         return sb.toString();
-    }
-    
-    private int factorial(int n) {
-        if (n <= 0) { return 1; }
-        int res = 1;
-        for (int i = n; i > 1; i--) {
-            res *= i;
-        }
-        return res;
     }
 }
