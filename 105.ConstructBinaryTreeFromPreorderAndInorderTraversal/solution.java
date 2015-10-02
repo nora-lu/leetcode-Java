a83771b57c3d88102645888689dc8459339b5672
nora-lu <nora-lu@users.noreply.github.com>
2015-10-01 22:53:26 -0400

    Update solution.java

--- 105.ConstructBinaryTreeFromPreorderAndInorderTraversal/solution.java
+++ 105.ConstructBinaryTreeFromPreorderAndInorderTraversal/solution.java
@@ -8,23 +8,21 @@
  * }
  */
 public class Solution {
-    private Map<Integer, Integer> map = new HashMap<>();
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder == null || inorder == null || preorder.length != inorder.length) {
-            return null;
-        }
-        for (int i = 0; i < inorder.length; i++) { map.put(inorder[i], i); }
-        return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length -1 );
-    }
-    
-    private TreeNode buildTree(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
-        if (ps > pe || is > ie) { return null; }
-        int rootVal = preorder[ps];
-        int posIn = is;
-        while (inorder[posIn] != rootVal) { posIn++; }
-        TreeNode root = new TreeNode(rootVal);
-        root.left = buildTree(preorder, ps + 1, ps + posIn - is, inorder, is, posIn - 1);
-        root.right = buildTree(preorder, ps + posIn - is + 1, pe, inorder, posIn + 1, ie);
-        return root;
-    }
+    private Map<Integer, Integer> nodeToPos = new HashMap<>();
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (preorder == null || inorder == null || preorder.length != inorder.length) {
+			return null;
+		}
+		for (int i = 0; i < inorder.length; i++) { nodeToPos.put(inorder[i], i); }
+		return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+	}
+
+	private TreeNode buildTree(int[] preorder, int ps, int pe, int[] inorder, int is, int ie) {
+		if (ps > pe || is > ie) { return null; }
+		int rootInorder = nodeToPos.get(preorder[ps]);
+		TreeNode root = new TreeNode(preorder[ps]);
+		root.left = buildTree(preorder, ps + 1, ps + rootInorder - is, inorder, is, rootInorder - 1);
+		root.right = buildTree(preorder, ps + rootInorder - is + 1, pe, inorder, rootInorder + 1, ie);
+		return root;
+	}
 }
