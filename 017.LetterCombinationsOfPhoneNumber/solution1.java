49ad5aa49bbe6cda81e0e0b8b72c2b479e850362
nora-lu <nora-lu@users.noreply.github.com>
2015-10-04 13:46:46 -0400

    Update solution1.java

--- 017.LetterCombinationsOfPhoneNumber/solution1.java
+++ 017.LetterCombinationsOfPhoneNumber/solution1.java
@@ -1,20 +1,21 @@
 public class Solution {
-    public List<String> letterCombinations(String digits) {
-        LinkedList<String> result = new LinkedList<String>();
-        if (digits == null || digits.equals("")) return result;
-        
-        String[] map = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        result.add("");
-        
-        for (int i = 0; i < digits.length(); i++) {
-            int x = Character.getNumericValue(digits.charAt(i));
-            while (result.peek().length() == i) {
-                String str = result.remove();
-                for (int j = 0; j < map[x].length(); j++) {
-                    result.add(str + map[x].charAt(j));
-                }
-            }
+  public List<String> letterCombinations(String digits) {
+    List<String> res = new LinkedList<>();
+    if (digits == null || digits.length() == 0) { return res; }
+    res.add("");
+    String[] map = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    
+    for (int i = 0; i < digits.length(); i++) {
+      int digit = (int)(digits.charAt(i) - '0');
+      int size = res.size();
+      for (int j = 0; j < size; j++) {
+        String pre = res.remove(0);
+        for (char c : map[digit].toCharArray()) {
+          res.add(pre + c);
         }
-        return result;
+      }
     }
+    return res;
+
+  }
 }
