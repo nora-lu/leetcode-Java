3f17576bb2ce008c50f92ea548157baa3f46bb8f
nora-lu <nora-lu@users.noreply.github.com>
2015-10-08 23:34:50 -0400

    Update solution.java

--- 066.PlusOne/solution.java
+++ 066.PlusOne/solution.java
@@ -1,3 +1,7 @@
+/*Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.*/
+
 public class Solution {
     public int[] plusOne(int[] digits) {
         if (digits == null || digits.length == 0) return digits;
