25b9ca6289d80b72c3f886d4df095e57591a754f
nora-lu <nora-lu@users.noreply.github.com>
2015-10-10 22:43:50 -0400

    Update mysolution.java

--- 041.FirstMissingPositive/mysolution.java
+++ 041.FirstMissingPositive/mysolution.java
@@ -1,3 +1,12 @@
+/* Given an unsorted integer array, find the first missing positive integer.
+
+For example,
+Given [1,2,0] return 3,
+and [3,4,-1,1] return 2.
+
+Your algorithm should run in O(n) time and uses constant space. */
+
+
 public class Solution {
     public int firstMissingPositive(int[] nums) {
         if (nums == null || nums.length == 0) { return 1; }
