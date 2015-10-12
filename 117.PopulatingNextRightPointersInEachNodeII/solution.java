4a1e8f1e517f54b8947dd05ff8af61d5c245055f
nora-lu <nora-lu@users.noreply.github.com>
2015-10-12 16:28:04 -0400

    Create solution.java

---
+++ 117.PopulatingNextRightPointersInEachNodeII/solution.java
@@ -0,0 +1,53 @@
+/* Follow up for problem "Populating Next Right Pointers in Each Node".
+
+What if the given tree could be any binary tree? Would your previous solution still work?
+
+Note:
+
+You may only use constant extra space.
+For example,
+Given the following binary tree,
+         1
+       /  \
+      2    3
+     / \    \
+    4   5    7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \    \
+    4-> 5 -> 7 -> NULL         */
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode head = null;  //The left most node in the lower level
+        TreeLinkNode prev = null;  //The previous node in the lower level
+        TreeLinkNode cur = root;  //The current node in the upper level
+        
+        while (cur != null) {
+            while (cur != null) {
+                if (cur.left != null) {
+                    if (prev != null) {
+                        prev.next = cur.left;
+                    } else {
+                        head = cur.left;
+                    }
+                    prev = cur.left;
+                }
+                if (cur.right != null) {
+                    if (prev != null) {
+                        prev.next = cur.right;
+                    } else {
+                        head = cur.right;
+                    }
+                    prev = cur.right;
+                }
+                cur = cur.next;
+            }
+            cur = head;
+            head = null;
+            prev = null;
+        }
+    }
+}
