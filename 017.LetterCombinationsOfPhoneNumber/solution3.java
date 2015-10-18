8ad3fc2a3fd3f84c1db82277dca67fcf5d69d8ac
nora-lu <nora-lu@users.noreply.github.com>
2015-10-18 13:22:48 -0400

    Added Backtracking solution.

---
+++ 017.LetterCombinationsOfPhoneNumber/solution3.java
@@ -0,0 +1,21 @@
+public class Solution {
+  private String[] map = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+  
+  public List<String> letterCombinations(String digits) {
+    List<String> res = new ArrayList<>();
+    recurse(digits.toCharArray(), "", 0, res);
+    return res;
+  }
+
+  private void recurse(char[] digits, String cur, int start, List<String> res) {
+    if (digits == null || digits.length == 0) { return; }
+    if (start == digits.length) {
+      res.add(cur);
+      return;
+    }
+    int digit = (int)(digits[start] - '0');
+    for (char c : map[digit].toCharArray()) {
+      recurse(digits, cur + c, start + 1, res);
+    }
+  } 
+}
