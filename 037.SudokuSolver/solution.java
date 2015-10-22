3c06165d6a1a17258203db5106c43e97e05fc73c
nora-lu <nora-lu@users.noreply.github.com>
2015-10-22 18:22:02 -0400

    Update solution.java

--- 037.SudokuSolver/solution.java
+++ 037.SudokuSolver/solution.java
@@ -1,3 +1,9 @@
+/* Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.'.
+
+You may assume that there will be only one unique solution. */
+
 public class Solution {
   public void solveSudoku(char[][] board) {
     if (board == null || board.length == 0) { return; }
