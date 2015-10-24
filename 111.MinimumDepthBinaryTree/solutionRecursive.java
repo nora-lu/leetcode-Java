6611bae620f0e5efecda19d2389938b00086feb4
nora-lu <nora-lu@users.noreply.github.com>
2015-10-23 23:10:21 -0400

    Update solutionRecursive.java

--- 111.MinimumDepthBinaryTree/solutionRecursive.java
+++ 111.MinimumDepthBinaryTree/solutionRecursive.java
@@ -8,10 +8,10 @@
  * }
  */
 public class Solution {
-    public int minDepth(TreeNode root) {
-        if (root == null) return 0;
-        else if (root.left == null && root.right == null) return 1;
-        else if (root.left != null && root.right != null) return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-        return root.left == null ? minDepth(root.right) + 1 : minDepth(root.left) + 1;
-    }
+  public int minDepth(TreeNode root) {
+    if (root == null) { return 0; }
+    if (root.left == null) { return minDepth(root.right) + 1; }
+    if (root.right == null) { return minDepth(root.left) + 1; }
+    return Math.min(minDepth(root.left), minDepth(root.right)) + 1; 
+  }
 }
