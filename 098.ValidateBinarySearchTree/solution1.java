09474f0f83e28290b1c86072bee2ef8427951826
nora-lu <nora-lu@users.noreply.github.com>
2015-10-24 13:11:02 -0400

    Update solution1.java

--- 098.ValidateBinarySearchTree/solution1.java
+++ 098.ValidateBinarySearchTree/solution1.java
@@ -1,3 +1,11 @@
+/* Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees. */
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -8,14 +16,15 @@
  * }
  */
 public class Solution {
-    public boolean isValidBST(TreeNode root) {
-        // be careful with node of Integer.MIN/MAX Value
-        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
-    }
-    
-    private static boolean isValidBST(TreeNode node, long min, long max) {
-        if (node == null) { return true; }
-        if (node.val >= max || node.val <= min) { return false; }
-        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
-    }
+  public boolean isValidBST(TreeNode root) {
+    return checkBST(root, null, null);
+  }
+
+  private boolean checkBST(TreeNode root, Integer min, Integer max) {
+    if (root == null) { return true; }
+    if ((min != null && root.val <= min) || (max != null && root.val >= max)) { return false; }
+    if (!checkBST(root.left, min, root.val) || !checkBST(root.right, root.val, max)) { return false; }
+    return true;
+  }
 }
+
