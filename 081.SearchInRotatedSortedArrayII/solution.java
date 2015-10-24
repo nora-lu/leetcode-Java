a0b4b9005eac13393ace626dcfbd599a9bf67368
nora-lu <nora-lu@users.noreply.github.com>
2015-10-24 13:53:28 -0400

    Update solution.java

--- 081.SearchInRotatedSortedArrayII/solution.java
+++ 081.SearchInRotatedSortedArrayII/solution.java
@@ -1,3 +1,10 @@
+/* Follow up for "Search in Rotated Sorted Array":
+What if duplicates are allowed?
+
+Would this affect the run-time complexity? How and why?
+
+Write a function to determine if a given target is in the array. */
+
 public class SearchInRotatedSortedArrayII {
   public boolean search(int[] nums, int target) {
     int lo = 0, hi = nums.length - 1;
