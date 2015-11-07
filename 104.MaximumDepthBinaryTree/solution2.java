8d41f2a4ff6bfee2b6e1d29e0f0f40b0587b2256
nora-lu <nora-lu@users.noreply.github.com>
2015-11-07 11:13:29 -0500

    BFS solution.

--- 104.MaximumDepthBinaryTree/solution2.java
+++ 104.MaximumDepthBinaryTree/solution2.java
@@ -1,1 +1,19 @@
-
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) { return 0; }
+        int depth = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            depth++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                if (node.left != null) { q.offer(node.left); }
+                if (node.right != null) { q.offer(node.right); }
+            }
+        }
+        return depth;
+    }
+}
