7a3c87c35558e15a2699305817c2527e836a5a80
nora-lu <nora-lu@users.noreply.github.com>
2015-11-07 22:58:24 -0500

    Update solution.java

--- 124.BinaryTreeMaximumPathSum/solution.java
+++ 124.BinaryTreeMaximumPathSum/solution.java
@@ -20,17 +20,20 @@ Return 6.  */
  * }
  */
 public class Solution {
-    private int max = Integer.MIN_VALUE;
+    public class Solution {
     public int maxPathSum(TreeNode root) {
-        helper(root);
-        return max;
+        int[] max = new int[1];
+        max[0] = Integer.MIN_VALUE;
+        maxPathSum(root, max);
+        return max[0];
     }
     
-    private int helper(TreeNode node) {
+    private int maxPathSum(TreeNode node, int[] max) {
         if (node == null) { return 0; }
-        int left = Math.max(0, helper(node.left));
-        int right = Math.max(0, helper(node.right));
-        max = Math.max(max, left + right + node.val);  // arch cannot be passed to parent node.
-        return Math.max(left, right) + node.val;
+        int left = Math.max(maxPathSum(node.left, max), 0);  // if negative
+        int right = Math.max(maxPathSum(node.right, max), 0);
+        max[0] = Math.max(max[0], left + right + node.val);  // once goes down in right subtree, cannot be passed to parent
+        return Math.max(left, right) + node.val; 
     }
+}
 }
