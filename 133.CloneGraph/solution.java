de60f0714469e89a0f0381e3a3c13281a58c17ae
nora-lu <nora-lu@users.noreply.github.com>
2015-11-08 08:45:40 -0500

    DFS solution.

--- 133.CloneGraph/solution.java
+++ 133.CloneGraph/solution.java
@@ -7,24 +7,45 @@
  * };
  */
 public class Solution {
-    private Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+    // private Map<Integer, UndirectedGraphNode> map = new HashMap<>();
+    
+    // public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+    //     if (node == null) { return null; }
+        
+    //     UndirectedGraphNode copyNode = new UndirectedGraphNode(node.label);
+    //     map.put(copyNode.label, copyNode);
+        
+    //     List<UndirectedGraphNode> copyNeighbors = copyNode.neighbors; 
+    //     for (UndirectedGraphNode neighbor : node.neighbors) {
+    //         if (map.containsKey(neighbor.label)) {
+    //           copyNeighbors.add(map.get(neighbor.label));         
+    //         } else {
+    //           UndirectedGraphNode copyNeighbor = cloneGraph(neighbor);
+    //           copyNeighbors.add(copyNeighbor);
+    //           map.put(copyNeighbor.label, copyNeighbor);
+    //         }
+    //     }
+    //     return copyNode;
+    // }
     
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if (node == null) { return null; }
-        
-        UndirectedGraphNode copyNode = new UndirectedGraphNode(node.label);
-        map.put(copyNode.label, copyNode);
-        
-        List<UndirectedGraphNode> copyNeighbors = copyNode.neighbors; 
-        for (UndirectedGraphNode neighbor : node.neighbors) {
-            if (map.containsKey(neighbor.label)) {
-               copyNeighbors.add(map.get(neighbor.label));         
-            } else {
-               UndirectedGraphNode copyNeighbor = cloneGraph(neighbor);
-               copyNeighbors.add(copyNeighbor);
-               map.put(copyNeighbor.label, copyNeighbor);
+        // old node->new node
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        UndirectedGraphNode copy = new UndirectedGraphNode(node.label);
+        map.put(node, copy);
+        dfs(node, map);
+        return copy;
+    }
+    
+    private void dfs(UndirectedGraphNode node, Map<UndirectedGraphNode, UndirectedGraphNode> map) {
+        for (UndirectedGraphNode cur : node.neighbors) {  // for each neighbor of old node
+            if (!map.containsKey(cur)) {  
+                UndirectedGraphNode copy = new UndirectedGraphNode(cur.label);
+                map.put(cur, copy);  
+                dfs(cur, map);  // clone current neighbor
             }
+            map.get(node).neighbors.add(map.get(cur));  // add copied neighbor to copied node
         }
-        return copyNode;
     }
 }
