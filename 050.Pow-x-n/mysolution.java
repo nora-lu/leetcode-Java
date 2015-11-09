25a807cee4ab51c6799901c3fe892bf16f0075de
nora-lu <nora-lu@users.noreply.github.com>
2015-11-09 10:33:59 -0500

    Update mysolution.java

--- 050.Pow-x-n/mysolution.java
+++ 050.Pow-x-n/mysolution.java
@@ -1,12 +1,13 @@
 public class Solution {
     public double myPow(double x, int n) {
         if (n == 0) { return 1; }
-        if (n == 1) { return x; }
-        if (n == -1) { return 1 / x; }
-        if (n % 2 != 0) { return myPow(x, n - 1) * x; }
-        else {
-            double sqr = myPow(x, n / 2);
-            return sqr * sqr;
+        if (n < 0) {
+            n = -n;
+            x = 1/x;
         }
+        if (n % 2 == 0) {
+            double sqrt = myPow(x, n / 2);
+            return sqrt * sqrt;
+        } else { return myPow(x, n - 1) * x; }
     }
 }
