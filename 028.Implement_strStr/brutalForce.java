62362488520e70859ac8aa0ad341ad9baec21dcd
nora-lu <nora-lu@users.noreply.github.com>
2015-11-08 22:27:53 -0500

    Update brutalForce.java

--- 028.Implement_strStr/brutalForce.java
+++ 028.Implement_strStr/brutalForce.java
@@ -1,3 +1,6 @@
+/* Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack. */
 public class Solution {
     public int strStr(String haystack, String needle) {
         if (haystack == null || needle == null) { return -1; }
