51ef1dd01049f332d9684d7b3c9e7c60b6483603
nora-lu <nora-lu@users.noreply.github.com>
2015-11-09 12:50:42 -0500

    Update solution.java

--- 062.UniquePaths/solution.java
+++ 062.UniquePaths/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
         if (m <= 0 || n <= 0) { return 0; }
-        int[] num_paths = new int[n + 1];
-        num_paths[1] = 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                num_paths[j + 1] += num_paths[j];
+                dp[j + 1] += dp[j];
             }
         }
-        return num_paths[n];
+        return dp[n];
     }
 }
