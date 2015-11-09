0590b94e7585d3d04e556ab029de4537d3de9c79
nora-lu <nora-lu@users.noreply.github.com>
2015-11-08 23:56:54 -0500

    Update solution.java

--- 038.CountAndSay/solution.java
+++ 038.CountAndSay/solution.java
@@ -1,18 +1,32 @@
+/* The count-and-say sequence is the sequence of integers beginning as follows:
+1, 11, 21, 1211, 111221, ...
+
+1 is read off as "one 1" or 11.
+11 is read off as "two 1s" or 21.
+21 is read off as "one 2, then one 1" or 1211.
+Given an integer n, generate the nth sequence.
+
+Note: The sequence of integers will be represented as a string. */
+
 public class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
-        String res = "";
-        String prev = countAndSay(n - 1);
-        int i = 0, count = 1;
-        while (i < prev.length() - 1) {
-            if (prev.charAt(i) == prev.charAt(i + 1)) count++;
-            else {
-                res = res + count + prev.charAt(i);
-                count = 1;
+        if (n <= 0) { return ""; }
+        String prev = "1";
+        for (int i = 2; i <= n; i++) {
+            StringBuilder cur = new StringBuilder();
+            char digit = prev.charAt(0);
+            int count = 1;
+            for (int j = 1; j < prev.length(); j++) {
+                if (prev.charAt(j) == digit) { count++; }
+                else {
+                    cur.append(count).append(digit);
+                    digit = prev.charAt(j);
+                    count = 1;
+                }
             }
-            i++;
+            cur.append(count).append(digit);
+            prev = cur.toString();
         }
-        res = res + count + prev.charAt(i);
-        return res.toString();
+        return prev;
     }
 }
