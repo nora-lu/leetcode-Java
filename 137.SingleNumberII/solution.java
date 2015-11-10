92702f1ca407a0add416f8ef05496cc95ec5e32d
nora-lu <nora-lu@users.noreply.github.com>
2015-11-10 07:54:28 -0500

    Update solution.java

--- 137.SingleNumberII/solution.java
+++ 137.SingleNumberII/solution.java
@@ -1,1 +1,20 @@
+/* Given an array of integers, every element appears three times except for one. Find that single one.
 
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? */
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int[] count = new int[32];
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                count[i] += (nums[j] >> i) & 1;
+            }
+        }
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            ret += ((count[i] % 3) << i);
+        }
+        return ret;
+    }
+}
