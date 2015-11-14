cd5123d8f8ca2fbe7936fcd2fa74d78abaedb749
nora-lu <nora-lu@users.noreply.github.com>
2015-11-14 18:02:40 -0500

    Update solution.java

--- 018.4Sum/solution.java
+++ 018.4Sum/solution.java
@@ -10,16 +10,15 @@ public class Solution {
                     if (j == i + 1 || nums[j] != nums[j - 1]) {
                         int lo = j + 1, hi = nums.length - 1, target2 = target - nums[i] - nums[j];
                         while (lo < hi) {
-                            if (nums[lo] + nums[hi] == target2) {
+                            int sum = nums[lo] + nums[hi];
+                            if (sum == target2) {
                                 res.add(Arrays.asList(nums[i], nums[j], nums[lo], nums[hi]));
-                                while (lo < hi && nums[lo] == nums[lo + 1]) { lo++; }
-                                while (lo < hi && nums[hi] == nums[hi - 1]) { hi--; }
-                                lo++;
-                                hi--;
-                            } else if(nums[lo] + nums[hi] < target2) {
+                            } 
+                            if(sum <= target2) {
                                 while (lo < hi && nums[lo] == nums[lo + 1]) { lo++; }
                                 lo++;
-                            } else {
+                            }  
+                            if (sum >= target2) {
                                 while (lo < hi && nums[hi] == nums[hi - 1]) { hi--; }
                                 hi--;
                             }
