14c56d63117006f652d5d6a83e14d5d0deebe4e8
nora-lu <nora-lu@users.noreply.github.com>
2015-11-14 10:49:20 -0500

    Update cleanersolution.java

--- 067.AddBinary/cleanersolution.java
+++ 067.AddBinary/cleanersolution.java
@@ -2,19 +2,13 @@ public class Solution {
     public String addBinary(String a, String b) {
         if (a == null || a.isEmpty()) { return b; }
         if (b == null || b.isEmpty()) { return a; }
-        
         StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() - 1;
-        int aByte = 0, bByte = 0, carry = 0;
-        while (i >= 0 || j >= 0 || carry == 1) {
-            aByte = (i >= 0) ? Character.getNumericValue(a.charAt(i--)) : 0;
-            bByte = (j >= 0) ? Character.getNumericValue(b.charAt(j--)) : 0;
-            /**
-             *  This is clever. 
-             */
-            int result = aByte ^ bByte ^ carry;
-            carry = aByte + bByte + carry >= 2 ? 1 : 0;
-            sb.append(result);
+        int carry = 0;
+        for (int i = a.length() - 1, j = b.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--) {
+            int digitA = (i >= 0) ? (a.charAt(i) - '0') : 0;
+            int digitB = (j >= 0) ? (b.charAt(j) - '0') : 0;
+            sb.append(digitA ^ digitB ^ carry);
+            carry = (digitA + digitB + carry) >= 2 ? 1 : 0;
         }
         return sb.reverse().toString();
     }
