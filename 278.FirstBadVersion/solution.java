17e67d79777482ab3ffb1f07e59ae7b2a41f3132
nora-lu <nora-lu@users.noreply.github.com>
2015-11-14 11:36:50 -0500

    Update solution.java

--- 278.FirstBadVersion/solution.java
+++ 278.FirstBadVersion/solution.java
@@ -3,14 +3,11 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        return binarySearch(n, 1, n);
-    }
-    
-    private int binarySearch(int n, int lo, int hi) {
+        int lo = 1, hi = n;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            if (isBadVersion(mid)) { hi = mid; }
-            else { lo = mid + 1; }
+            if (!isBadVersion(mid)) { lo = mid + 1; }
+            else { hi = mid; }
         }
         return lo;
     }
