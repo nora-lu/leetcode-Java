9031807915c34df795063b52f136a38202b84cae
nora-lu <nora-lu@users.noreply.github.com>
2015-11-14 20:08:32 -0500

    Update cleanersolution.java

--- 043.MultiplyStrings/cleanersolution.java
+++ 043.MultiplyStrings/cleanersolution.java
@@ -1,23 +1,22 @@
 public class Solution {
     public String multiply(String num1, String num2) {
-        int n1 = num1.length(), n2 = num2.length();
-        int[] products = new int[n1 + n2];
-        for (int i = n1 - 1; i >= 0; i--) {
-            for (int j = n2 - 1; j >= 0; j--) {
-                int d1 = num1.charAt(i) - '0';
-                int d2 = num2.charAt(j) - '0';
-                products[i + j + 1] += d1 * d2;
+        int m = num1.length(), n = num2.length();
+        int[] ret = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                ret[i + j + 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
             }
         }
         int carry = 0;
-        for (int i = products.length - 1; i >= 0; i--) {
-            int tmp = (products[i] + carry) % 10;
-            carry = (products[i] + carry) / 10;
-            products[i] = tmp;
+        for (int i = m + n - 1; i >= 0; i--) {
+            int sum = carry + ret[i];
+            ret[i] = sum % 10;
+            carry = sum / 10;
         }
         StringBuilder sb = new StringBuilder();
-        for (int num : products) sb.append(num);
-        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
-        return sb.length() == 0 ? "0" : sb.toString();
+        int i = 0;
+        while (i < m + n - 1 && ret[i] == 0) { i++; }
+        while (i < m + n) { sb.append(ret[i++]); }
+        return sb.toString();
     }
 }
