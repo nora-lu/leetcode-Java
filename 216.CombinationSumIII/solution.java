26abe93ea472f37e9fec5485e0b12585bac44174
nora-lu <nora-lu@users.noreply.github.com>
2015-11-15 10:44:20 -0500

    Update solution.java

--- 216.CombinationSumIII/solution.java
+++ 216.CombinationSumIII/solution.java
@@ -1,23 +1,20 @@
 public class Solution {
-    private List<List<Integer>> res = new ArrayList<>();
-    private List<Integer> list = new ArrayList<>();
-    
-    public List<List<Integer>> combinationSum3(int k, int n) {
-        helper(k, n, 1);
+  public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(k, n, 1, res, new ArrayList<Integer>());
         return res;
     }
-    
-    private void helper(int k, int sum, int start) {
+
+    private void helper(int k, int sum, int start, List<List<Integer>> res, List<Integer> list) {
         if (sum == 0 && list.size() == k) {
             res.add(new ArrayList<Integer>(list));
-        } else {
-            for (int i = start; i <= 9; i++) {
-               if (sum - i >= 0) {
-                   list.add(i);
-                   helper(k, sum - i, i + 1);
-                   list.remove(list.size() - 1);
-               } else { break; }
-            }
+            return;
         }
-    }
+        for (int i = start; i <= 9; i++) {
+            if (sum - i >= 0) {
+                list.add(i);
+                helper(k, sum - i, i + 1, res, list);
+                list.remove(list.size() - 1);
+            } else { break; }
+        }
 }
