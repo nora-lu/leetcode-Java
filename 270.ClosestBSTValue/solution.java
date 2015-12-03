71ca85df1e9a11ccd1a8ef54291ce5a7769db1f2
nora-lu <nora-lu@users.noreply.github.com>
2015-12-03 16:41:58 -0500

    Update solution.java

--- 270.ClosestBSTValue/solution.java
+++ 270.ClosestBSTValue/solution.java
@@ -1,20 +1,4 @@
 public class ClosestBSTValue {
-
-  // Iterative solution.
-  public int closestValue(TreeNode root, double target) {
-    if (root == null) { return Integer.MAX_VALUE; }
-    TreeNode tmp = root;
-    int closest = tmp.val;
-    while (tmp != null) {
-      if (tmp.val == target) { return tmp.val; }
-      if (Math.abs(tmp.val - target) < Math.abs(closest - target)) { closest = tmp.val; }
-      if (target < tmp.val) { tmp = tmp.left; }
-      else { tmp = tmp.right; }  
-    }
-    return closest;
-  }
-
-
   // Recursive solution.
   public int closestValue(TreeNode root, double target) {
     TreeNode child = target < root.val ? root.left : root.right;
