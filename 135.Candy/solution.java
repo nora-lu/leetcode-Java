01a96ddfc26d8b27ca4d747a7727403092d8facd
nora-lu <nora-lu@users.noreply.github.com>
2015-12-03 14:09:05 -0500

    Update solution.java

--- 135.Candy/solution.java
+++ 135.Candy/solution.java
@@ -1,25 +1,24 @@
 public class Solution {
     public int candy(int[] ratings) {
-        if (ratings == null) { return 0; }
+        if (ratings == null || ratings.length == 0) { return 0; }
         
-        int n = ratings.length;
-        int[] candy = new int[n];
+        int len = ratings.length;
+        int[] candy = new int[len];
         Arrays.fill(candy, 1);
         
         // scan from left, compare with left neighbor
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < len; i++) {
             if (ratings[i] > ratings[i - 1]) { candy[i] = candy[i - 1] + 1; }
         }
         
-        // scan from right, compare with right neighbor
-        for (int i = n - 2; i >= 0; i--) {
+        int min = candy[len - 1];
+        // scan from right, compare with right neighbor and compute the total
+        for (int i = len - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candy[i] = Math.max(candy[i], candy[i + 1] + 1);
             }
+            min += candy[i];
         }
-        
-        int min = 0;
-        for (int x : candy) { min += x; }
         return min;
     }
 }
