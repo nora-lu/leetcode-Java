d27c0cd115fc1e142d2778cd92adbb2791ddafa3
nora-lu <nora-lu@users.noreply.github.com>
2015-12-03 11:35:03 -0500

    Update cleanSolution.java

--- 102.BinaryTreeLevelOrderTraversal/cleanSolution.java
+++ 102.BinaryTreeLevelOrderTraversal/cleanSolution.java
@@ -1,21 +1,21 @@
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-
-        if(root == null) return wrapList;
-
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int levelNum = queue.size();
-            List<Integer> subList = new LinkedList<Integer>();
-            for(int i=0; i<levelNum; i++) {
-                if(queue.peek().left != null) queue.offer(queue.peek().left);
-                if(queue.peek().right != null) queue.offer(queue.peek().right);
-                subList.add(queue.poll().val);
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) { return ret; }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if (node.left != null) { q.offer(node.left); }
+                if (node.right != null) { q.offer(node.right); }
             }
-            wrapList.add(subList);
+            ret.add(level);
         }
-        return wrapList;
+        return ret;
     }
 }
