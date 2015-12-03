eac7f7ed00609cc34664a0e7dea99d722a54f076
nora-lu <nora-lu@users.noreply.github.com>
2015-12-03 08:49:00 -0500

    Update mysolution.java

--- 016.3SumClosest/mysolution.java
+++ 016.3SumClosest/mysolution.java
@@ -3,6 +3,7 @@ public class Solution {
         Arrays.sort(nums);
         int closest = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { continue; }
             int lo = i + 1, hi = nums.length - 1;
             while (lo < hi) {
                 int sum = nums[i] + nums[lo] + nums[hi];
