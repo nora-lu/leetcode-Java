0651b4681277c716beb3421746ad061b2c29c348
nora-lu <nora-lu@users.noreply.github.com>
2015-12-05 16:09:50 -0500

    Update solution.java

--- 154.FindMinimumInRotatedSortedArrayII/solution.java
+++ 154.FindMinimumInRotatedSortedArrayII/solution.java
@@ -10,4 +10,17 @@ public class Solution {
         }
         return nums[lo];
     }
+    
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) { return 0; }
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            if (nums[lo] < nums[hi]) { return nums[lo]; }
+            int mid = lo + (hi - lo) / 2;
+            if (nums[lo] < nums[mid]) { lo = mid + 1; }
+            else if (nums[lo] > nums[mid]) { hi = mid; }
+            else { lo++; }
+        }
+        return nums[lo];
+    }
 }
