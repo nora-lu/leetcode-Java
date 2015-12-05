400c4472100867a411b8fcd728a23c420fb6ebad
nora-lu <nora-lu@users.noreply.github.com>
2015-12-05 12:21:24 -0500

    Update solution2.java

--- 241.DifferentWaysToAddParentheses/solution2.java
+++ 241.DifferentWaysToAddParentheses/solution2.java
@@ -1,37 +1,37 @@
 public class Solution {
-    private static Map<String, List<Integer>> cache = new HashMap<>();
-    
     public List<Integer> diffWaysToCompute(String input) {
+        return compute(input, new HashMap<String, List<Integer>>());
+    }
+    
+    private List<Integer> compute(String input, Map<String, List<Integer>> cache) {
         if (cache.get(input) != null) { return cache.get(input); }
         
-        List<Integer> res = new ArrayList<>();
+        List<Integer> ret = new ArrayList<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
             if (c == '+' || c == '-' || c == '*') {
-                String left = input.substring(0, i);
-                String right = input.substring(i + 1);
-                List<Integer> ltlist = diffWaysToCompute(left);
-                List<Integer> rtlist = diffWaysToCompute(right);
-                for (Integer lt : ltlist) {
-                    for (Integer rt : rtlist) {
-                        int n = 0;
+                List<Integer> left = compute(input.substring(0, i), cache);
+                List<Integer> right = compute(input.substring(i + 1), cache);
+                for (int x : left) {
+                    for (int y : right) {
                         switch (c) {
-                            case '+': n = lt + rt;
+                            case '+':
+                                ret.add(x + y);
                                 break;
-                            case '-': n = lt - rt;
+                            case '-':
+                                ret.add(x - y);
                                 break;
-                            case '*': n = lt * rt;
+                            case '*':
+                                ret.add(x * y);
                                 break;
                         }
-                        res.add(n);
+                            
                     }
                 }
             }
         }
-        if (res.size() == 0) {  // Don't forget!!! Input is just number, no operators.
-            res.add(Integer.valueOf(input));
-        }
-        cache.put(input, res);
-        return res;
+        if (ret.size() == 0) { ret.add(Integer.parseInt(input)); }  // when input does not have operators!
+        cache.put(input, ret);
+        return ret;
     }
 }
