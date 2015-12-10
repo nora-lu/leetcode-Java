6f11328b165369f0e2a0ecc3bf5beadbbd378600
nora-lu <nora-lu@users.noreply.github.com>
2015-12-10 16:38:18 -0500

    Update solution2.java

--- 125.ValidPalindrome/solution2.java
+++ 125.ValidPalindrome/solution2.java
@@ -7,13 +7,19 @@ public class Solution {
         while (lo < hi) {
             while (lo < hi && !isValid(s.charAt(lo))) { lo++; }
             while (lo < hi && !isValid(s.charAt(hi))) { hi--; }
-            if (s.charAt(lo) != s.charAt(hi) && Math.abs(s.charAt(lo) - s.charAt(hi)) != check) { return false; }
-            lo++; hi--; 
+            char l = s.charAt(lo), h = s.charAt(hi);
+            if (isDigit(l) != isDigit(h)) { return false; }
+            if (l != h && Math.abs(l - h) != check) { return false; }
+            lo++; hi--;
         }
         return true;
     }
 
     public boolean isValid(char a) {
         return (a - 'A' >= 0 && a - 'Z' <= 0) || (a - 'a' >= 0 && a - 'z' <= 0) || (a - '0' >= 0 && a - '9' <= 0);
+    }
+
+    public boolean isDigit(char a) {
+        return (a - '0' >= 0 && a - '9' <= 0);
     }
 }
